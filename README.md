c74f70c266e3f0167d58c74c57c7da3e53c7ae79
Lars Mescheder <larsmescheder@gmx.net>
2016-02-28 19:10:37 +0100

    Make radon transform work + add readme.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Radon
-
 [![Build Status](https://travis-ci.org/LMescheder/Radon.jl.svg?branch=master)](https://travis-ci.org/LMescheder/Radon.jl)
+
+This package contains routines to compute the [Radon transform](https://en.wikipedia.org/wiki/Radon_transform) of a 2D image and the corresponding back projection. In the future, it might also contain algorithms to invert the Radon transform.
+
+*Note*: this package is in an early stage and presumably subject to substantial change in the future.
--- src/Radon.jl
+++ src/Radon.jl
@@ -1,5 +1,78 @@
 module Radon
-
+export radon!
 # package code goes here
+
+function radon{T<:AbstractFloat}(im::AbstractArray{T, 2})
+    Nx, Ny = size(im)
+    radonim = similar(im, min(Nx, Ny), max(Nx, Ny))
+    radon!(radonim, im)
+    radonim
+end
+
+function radon!{T<:AbstractFloat}(radonim::AbstractArray{T, 2}, im::AbstractArray{T, 2})
+    Nφ, Ns = size(radonim)
+    Nx, Ny = size(im)
+    Ldiag = hypot(Nx, Ny)
+    K = round(Int, Ldiag)
+
+    dLz = Ldiag/(K-1)
+    dLs = Ldiag/(Ns-1)
+    dφ = π/Nφ
+    for is = 1:Ns, iφ = 1:Nφ
+        φ = (iφ-1)*dφ
+        s = -Ldiag/2 + (is-1)*dLs
+        tx = cos(φ)
+        ty = sin(φ)
+
+        radonim[iφ, is] = zero(T)
+        for k=1:K
+            z = -Ldiag/2 + (k-1)*dLz
+            x = round(Int, Nx/2 + z*ty + s*tx)
+            y = round(Int, Ny/2 - z*tx + s*ty)
+
+            if 1 <= x <= Nx && 1 <= y <= Ny
+                radonim[iφ, is] += im[x, y]*dLz
+            end
+        end
+    end
+    radonim
+end
+
+function backproject{T<:AbstractFloat}(radonim::AbstractArray{T, 2})
+    Nφ, Ns = size(radonim)
+    im = similar(radonim, Ns, Ns)
+    backproject!(im, radonim)
+    im
+end
+
+function backproject!{T<:AbstractFloat}(im::AbstractArray{T, 2}, radonim::AbstractArray{T, 2})
+    Nφ, Ns = size(radonim)
+    Nx, Ny = size(im)
+    Ldiag = hypot(Nx, Ny)
+    K = round(Int, Ldiag)
+
+    dLz = Ldiag/(K-1)
+    dLs = Ldiag/(Ns-1)
+    dφ = π/Nφ
+
+    im[:] = 0.
+    for is = 1:Ns, iφ = 1:Nφ
+        φ = (iφ-1)*dφ
+        s = -Ldiag/2 + (is-1)*dLs
+        tx = cos(φ)
+        ty = sin(φ)
+
+        for k=1:K
+            z = -Ldiag/2 + (k-1)*dLz
+            x = round(Int, Nx/2 + z*ty + s*tx)
+            y = round(Int, Ny/2 - z*tx + s*ty)
+
+            if 1 <= x <= Nx && 1 <= y <= Ny
+                im[x, y] += radonim[iφ, is]*dLz
+            end
+        end
+    end
+    im
+end
 
 end # module
